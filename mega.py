efc507a16be3416e48acabaffb14cc014f813828
Evan D <edogariu@princeton.edu>
2022-10-28 06:21:13 -0400

    fix mega

--- mega.py
+++ mega.py
@@ -22,7 +22,11 @@ SPLIT_INTERVALS = {'train': (0, 0.85),  # intervals for each split
                    'all': (0, 1.0)}
 
 class MegaDataset(D.Dataset):
-    def __init__(self, mode: str, idxs_to_use: List[int]):
+    _pca = None
+    _inputs = None
+    _targets = None
+    
+    def __init__(self, split: str, mode: str, idxs_to_use: List[int]):
         """
         dataset for pca + mel-spectrogram-ified inference
 
@@ -34,39 +38,22 @@ class MegaDataset(D.Dataset):
             which indices to use in the dataset
         """
         
-        assert mode in ['multi', 'cite']
-        
-        self.pca = np.load(f'data/train_{mode}_inputs_pca.npy') if mode == 'multi' else np.concatenate((np.load(f'data/train_{mode}_inputs_coding.npy'), np.load(f'data/train_{mode}_inputs_pca.npy')), axis=-1)
-        self.inputs = ss.load_npz(f'data_sparse/train_{mode}_inputs_sparse.npz')[idxs_to_use]
-        self.targets = np.load(f'data/train_{mode}_targets_pca.npy')[idxs_to_use] if mode == 'multi' else pd.read_hdf(f'data/train_{mode}_targets.h5').values
-        # self.targets = ss.load_npz(f'data_sparse/train_{mode}_targets_sparse.npz')[idxs_to_use]
-        
-        assert self.inputs.shape[0] == self.targets.shape[0] and self.inputs.shape[0] == self.pca.shape[0]
-        self.length = self.inputs.shape[0]
-        
-        self.idxs = np.arange(self.length)
-        self.split = None
-    
-    def __len__(self):
-        return self.length
-    
-    def __getitem__(self, index: int):
-        index = self.idxs[index]
-        pca = self.pca[index]
-        in_seq = self.inputs[index].toarray()[0]  # could add more to inputs here
-        mel = librosa.power_to_db(librosa.feature.melspectrogram(y=in_seq, sr=len(in_seq)))
-        targets = self.targets[index]
-        
-        return (pca, mel, targets)
-                
-    def get_dataloader(self, split: str, batch_size: int, 
-                       pin_memory=True, num_workers=0, shuffle=True, drop_last=True):
-        
         assert split in ['train', 'val', 'test', 'all']
+        
+        assert mode in ['multi', 'cite']
+        self.mode = mode
+        
+        if not MegaDataset._pca is not None: 
+            MegaDataset._pca = np.load(f'data/train_{mode}_inputs_pca.npy')[idxs_to_use] if mode == 'multi' else np.concatenate((np.load(f'data/train_{mode}_inputs_coding.npy'), np.load(f'data/train_{mode}_inputs_pca.npy')), axis=-1)[idxs_to_use]
+        if not MegaDataset._inputs is not None: 
+            MegaDataset._inputs = pd.read_hdf('data/train_cite_inputs.h5').values[idxs_to_use] # self.inputs = ss.load_npz(f'data_sparse/train_{mode}_inputs_sparse.npz')[idxs_to_use]
+        if not MegaDataset._targets is not None: 
+            MegaDataset._targets = np.load(f'data/train_{mode}_targets_pca.npy')[idxs_to_use] if mode == 'multi' else pd.read_hdf(f'data/train_{mode}_targets.h5').values[idxs_to_use]
+        # self.targets = ss.load_npz(f'data_sparse/train_{mode}_targets_sparse.npz')[idxs_to_use]
         
         # create correct split       
         np.random.seed(0)  # to ensure same train, val, test splits every time
-        length = self.length
+        length = len(idxs_to_use)
         random_idxs = np.random.permutation(length)
         start, stop = SPLIT_INTERVALS[split]
         start, stop = int(start * length), int(stop * length)
@@ -76,6 +63,23 @@ class MegaDataset(D.Dataset):
         self.idxs = idxs
         self.length = len(idxs)
         
+        assert MegaDataset._inputs.shape[0] == MegaDataset._targets.shape[0] and MegaDataset._inputs.shape[0] == MegaDataset._pca.shape[0]
+        
+    def __len__(self):
+        return self.length
+    
+    def __getitem__(self, index: int):
+        index = self.idxs[index]
+        pca = MegaDataset._pca[index]
+        in_seq = MegaDataset._inputs[index] # in_seq = self.inputs[index].toarray()[0]  # could add more to inputs here
+        mel = librosa.feature.melspectrogram(y=in_seq, sr=len(in_seq), hop_length=1800 if self.mode == 'multi' else 173)  # hop lengths to ensure (128 x 128) spectrograms
+        if self.mode == 'cite': mel = librosa.power_to_db(mel)
+        targets = MegaDataset._targets[index]
+        
+        return (pca, mel, targets)
+                
+    def get_dataloader(self, batch_size: int, 
+                       pin_memory=True, num_workers=0, shuffle=True, drop_last=True):
         return D.DataLoader(self, batch_size, shuffle=shuffle, drop_last=drop_last, pin_memory=pin_memory, num_workers=num_workers)
 
 class MelHead(nn.Module):
@@ -101,7 +105,7 @@ class MelHead(nn.Module):
         tower_layers = [nn.Unflatten(1, (1, mel_shape[0]))]
         for i in range(tower_depth):
             in_chan, out_chan = tower_channels[i: i + 2]
-            layer = [nn.Conv2d(in_chan, out_chan, kernel_size=(5, 3))]
+            layer = [nn.Conv2d(in_chan, out_chan, kernel_size=(3, 3))]
             if (i + 1) % pool_every == 0:
                 layer.append(pools[pooling_type](out_chan, pool_size))
             layer.append(nn.ReLU())
@@ -213,8 +217,8 @@ class MegaModel(ModelWrapper):
         h2 = self.mel_head(mel)
         cat = torch.cat((h1, h2), dim=1)
         pred = self.fpn(cat)
-        # loss = losses.negative_correlation_loss(pred, y)
-        loss = F.mse_loss(pred, y[:, :self.out_dim])
+        loss = losses.negative_correlation_loss(pred, y)
+        # loss = F.mse_loss(pred, y[:, :self.out_dim])
         return loss
     
     def eval_err(self, 
@@ -226,27 +230,26 @@ class MegaModel(ModelWrapper):
             h2 = self.mel_head(mel)
             cat = torch.cat((h1, h2), dim=1)
             pred = self.fpn(cat)
-            # loss = -correlation_score(pred.cpu().numpy(), y.cpu().numpy())
-            loss = F.mse_loss(pred, y[:, :self.out_dim]).item()
+            loss = -correlation_score(pred.cpu().numpy(), y.cpu().numpy())
+            # loss = F.mse_loss(pred, y[:, :self.out_dim]).item()
             error = loss
         return error, loss  
 
 if __name__ == '__main__':
     # ------------------------------------- hyperparameters -------------------------------------------------
 
-    mode = 'multi'; assert mode in ['multi', 'cite']
+    mode = 'cite'; assert mode in ['multi', 'cite']
     model_name = f'mega_{mode}'
     batch_size = 128
 
     trainer_args = {'initial_lr': 0.03,
-                    'lr_decay_period': 5,
+                    'lr_decay_period': 10,
                     'lr_decay_gamma': 0.7,
                     'weight_decay': 0.0002}
-    train_args = {'num_epochs': 50,
-                  'eval_every': 3,
+    train_args = {'num_epochs': 60,
+                  'eval_every': 2,
                   'patience': 3,
                   'num_tries': 4}
-    
     
     # multi
     if mode == 'multi':
@@ -258,7 +261,7 @@ if __name__ == '__main__':
                     'hidden_dim': 512, 
                     'out_dim': 4000, 
                     'depth': 4}
-        mel_args = {'mel_shape': (128, 448),
+        mel_args = {'mel_shape': (128, 128),
                     'out_dim': 4000,
                     'n_chan': 128,
                     'tower_depth': 8,
@@ -272,32 +275,32 @@ if __name__ == '__main__':
     else:
         fpn_args = {'hidden_dim': 512,
                     'out_dim': 140,
-                    'body_depth': 4,
+                    'body_depth': 6,
                     'dropout': 0.1}
         pca_args = {'in_dim': 4110, 
                     'hidden_dim': 512, 
-                    'out_dim': 500, 
+                    'out_dim': 512, 
                     'depth': 5}
-        mel_args = {'mel_shape': (128, 44),
-                    'out_dim': 500,
-                    'n_chan': 128,
-                    'tower_depth': 6,
+        mel_args = {'mel_shape': (128, 128),
+                    'out_dim': 512,
+                    'n_chan': 256,
+                    'tower_depth': 10,
                     'body_type': 'linear',
                     'body_depth': 4,
                     'pooling_type': 'max',
                     'pool_every': 2,
                     'pool_size': 2,
-                    'dropout': 0.05}
+                    'dropout': 0.1}
         
 
     # --------------------------------------------------------------------------------------------------------
 
     print('preparing datasets')
     idxs = get_train_idxs(mode)
-    dataset = MegaDataset(mode, idxs)
-    # dataset2 = MegaDataset(mode, idxs)
-    train_dataloader = dataset.get_dataloader('train', batch_size)
-    val_dataloader = None# dataset2.get_dataloader('val', batch_size)
+    train_dataset = MegaDataset('train', mode, idxs)
+    val_dataset = MegaDataset('val', mode, idxs)
+    train_dataloader = train_dataset.get_dataloader(batch_size)
+    val_dataloader = val_dataset.get_dataloader(batch_size)
     
     print('training')
     model = MegaModel(model_name, **fpn_args, pca_args=pca_args, mel_args=mel_args)
